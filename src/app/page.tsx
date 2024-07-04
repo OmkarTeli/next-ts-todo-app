defad5efc706197a921ad1e0e06031072065b02d
OmkarTeli <omkarteli8055@gmail.com>
2024-07-04 17:51:57 +0530

    feat/ new header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
    return (
       <main>
+         <h1>New header added</h1>
          <h2>TODO NEXT + TYPESCRIPT APP</h2>
          <AddTodo />
       </main>
